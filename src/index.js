10c712b64886d5e1455d93e114fdada2bc2a8157
Khushal Singh <98296678+khushalsing814@users.noreply.github.com>
2023-07-29 14:01:41 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+const jsonServer = require("json-server"); // importing json-server library
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
+
+server.use(middlewares);
+server.use(router);
+server.listen(port);
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
